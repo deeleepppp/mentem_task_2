4be5db71f01d6cba8780dd7a22d8815c6b33c789
deeleepppp <kr.dilipsinghh@gmail.com>
2025-03-26 16:46:42 +0530

    solve drag and drop issue

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 
 const Task = ({ id, title, handleDelete }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "TASK",
+    item: { id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }))
   return (
-    <div className=" flex item-center mb-2 justify-between h-fit w-full bg-gray-200 px-1 py-2 rounded-lg">
-      <h3 className="font-semibold font-gilroy">{title}</h3>
+    <div ref={drag} className=" flex item-center mb-2 justify-between h-fit w-full bg-gray-200 px-1 py-2 rounded-lg">
+      <h3 className="font-bold font-gilroy">{title}</h3>
       <button onClick={() => handleDelete(id)} className="bg-red-600 text-sm text-white rounded-full h-6 w-6 ">
         X
       </button>
